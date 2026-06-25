333e28cbd7c14df628a875d6ac908a6d74ff7060
Sangpham26 <sangpham0231@gmail.com>
2026-06-25 20:55:32 +0700

    update: connect mogoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,14 @@
-
-// const express = require('express');
+import dns from "node:dns/promises";
+dns.setServers(["1.1.1.1", "1.0.0.1"]); 
 import express from 'express';
 import "dotenv/config";
+import User from './models/user.model.js';
+import Message from './models/message.model.js';
+import { connectDB } from './lib/db.js';
+
 const app = express();
 const PORT = process.env.PORT;
-console.log(process.env.MONGO_URL);
-app.listen(PORT, () => {console.log('Server is running on port ' + PORT)});
+app.listen(PORT, () => {
+    connectDB();
+    console.log('Server is running on port ' + PORT)
+});
